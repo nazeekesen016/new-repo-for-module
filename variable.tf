76a76e0a02ccca410d8cdc1b16ea225e03e1114f
nazeekesen016 <nasik2431@gmail.com>
2023-02-02 16:02:07 +0600

    initial commit

---
+++ variable.tf
@@ -0,0 +1,20 @@
+variable "subnet-name" {
+  default = ""
+}
+
+variable "subnets-cidr-block" {
+  default = ""
+}
+
+variable "vpc-cidr-block" {
+  default = ""
+}
+
+variable "name" {
+  type    = string
+  default = ""
+}
+
+variable "subnet-id" {
+  default = ""
+}
